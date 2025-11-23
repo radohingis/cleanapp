8ae5b31ae9c8b5cb63fd1b28403d51d963f2b350
radohingis <radoslav.hingis@lifted.dev>
2025-11-23 17:10:53 +0100

    updated header and user util dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,32 @@
 import useAuthStore from "@/stores/auth.store"
 import { Button } from "./ui/button"
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import AuthDialog from "@/components/auth/AuthDialog"
 import { ModeToggle } from "./ui/mode-toggle"
+import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
 
 
 function Header() {
-  const { user } = useAuthStore()
+  const { user, signOut } = useAuthStore()
 
   return <header className="bg-accent">
     <div className="container mx-auto p-4 flex items-center gap-4">
       <h1 className="text-xl font-bold text-primary">CleanApp</h1>
       <div className="ml-auto"></div>
       {!user
-        ? <AuthDialog trigger={<Button>Sign In</Button>} /> 
-        : <Avatar>
-            <AvatarFallback>{user?.email?.charAt(0).toUpperCase() ?? "X"}</AvatarFallback>
-        </Avatar>
+        ? <AuthDialog trigger={<Button size="sm">Sign In</Button>} /> 
+        : <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="rounded-full" size="sm">{user.email?.charAt(0)}</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()}>
+              Sign Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       }
       <ModeToggle />
     </div>
